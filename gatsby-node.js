b0e093f25a51c5763fb877484ee38e0c03185f0e
Clinton <clinton.langosch@gmail.com>
2022-09-21 14:20:15 -0700

    wip: create custom types, source nodes, and simple output on home page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,49 @@
+const ARTWORK_SLIDES_PATH_PREFIX = "src/artwork-slides/";
+const path = require("path");
+
+exports.sourceNodes = ({
+  actions,
+  createNodeId,
+  createContentDigest,
+  getNodesByType,
+}) => {
+  const { createNode } = actions;
+
+  const slides = getNodesByType("MarkdownRemark");
+
+  slides
+    .filter((slideNode) =>
+      // Ensure this works in other file dirs like on netlify
+      slideNode.fileAbsolutePath.startsWith(
+        path.join(`${process.cwd()}/${ARTWORK_SLIDES_PATH_PREFIX}`)
+      )
+    )
+    .forEach((slideNode, index) => {
+      const { frontmatter, fileAbsolutePath } = slideNode;
+      const data = {
+        title: frontmatter.title,
+        year: frontmatter.year,
+        artForm: frontmatter.artForm || null,
+        imageFileName: frontmatter.imageFileName,
+        caption: frontmatter.caption || null,
+      };
+      const filename = fileAbsolutePath.substring(
+        fileAbsolutePath.lastIndexOf("/") + 1
+      );
+
+      createNode({
+        id: createNodeId(`slide-${index}-${filename}`),
+        parent: null,
+        children: [],
+        internal: {
+          type: "ArtworkSlide",
+          contentDigest: createContentDigest(data),
+        },
+        ...data,
+      });
+    });
+};
+
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions;
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import * as React from "react";
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
 
-const HomePage = () => {
+const HomePage = ({ data }) => {
+  const {
+    allArtworkSlide: { nodes: slides },
+  } = data;
   return (
     <>
       <head>
@@ -13,7 +18,45 @@ const HomePage = () => {
         <li>Contact</li>
         <li>CV</li>
       </ul>
-      <div>Artwork carousel here</div>
+      <div>Artwork slides here</div>
+
+      {slides.map((slide) => (
+        <>
+          <ul>
+            <li>{slide.title}</li>
+            <li>{slide.year}</li>
+            <li>{slide.artForm}</li>
+            <li>{slide.caption}</li>
+            <li>
+              image here
+              {/* <Img fluid={slide.imageFileName.childImageSharp.fluid} /> */}
+            </li>
+          </ul>
+        </>
+      ))}
     </>
   );
 };
+
+export default HomePage;
+
+export const query = graphql`
+  query SlideQuery {
+    allArtworkSlide {
+      nodes {
+        artForm
+        caption
+        # imageFileName {
+        #   childImageSharp {
+        #     fluid(maxWidth: 800) {
+        #       ...GatsbyImageSharpFluid
+        #     }
+        #   }
+        # }
+        id
+        title
+        year
+      }
+    }
+  }
+`;
