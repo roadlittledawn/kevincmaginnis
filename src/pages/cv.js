a6f15d60238f4ab2310fd2937a523c6e4415350c
Kat Jarboe <jarboek1@gmail.com>
2023-02-27 21:28:47 -0600

    add cv page and link in sidebar.js

---
+++ src/pages/cv.js
@@ -0,0 +1,93 @@
+/** @jsx jsx */
+import * as React from "react";
+import { jsx } from "@emotion/react";
+
+const CvPage = () => {
+    return (
+        <>
+        <div>
+            <h3>Born</h3>
+            <p>
+                1955 Menominee, Michigan
+            </p>
+        </div>
+
+        <div>
+            <h3>Education</h3>
+            <p>
+                1977 B.A. University of Wisconsin at Green Bay
+            </p>
+        </div>
+
+        <div>
+            <h3>Metier</h3>
+            <p>
+                <ul>
+                    <li>2005-present, artist self-employed</li>
+                    <li>2000-2004 President, MDI</li>
+                    <li>1991-1999 President, Maginnis, Inc.</li>
+                    <li>1979-1998 President, Maginnis Graphics, Inc.</li>
+                </ul>
+            </p>
+        </div>
+
+        <div>
+            <h3>Curriculum Vitae</h3>
+            <h4>Solo Exhibitions</h4>
+                <ul>
+                    <li>2019 "Black", Ignition Projects Gallery, Chicago, Illinois</li>
+                    <li>2016 "Spun", Stuart and Co. Gallery, Chicago, Illinois (catalogue)</li>
+                    <li>2015 "Untitled", The Suburban, Oak Park, Illinois</li>
+                    <li>1990 Rezac Gallery,  Chicago, Illinois (catalogue)</li>
+                    <li>1986 "Science as Symbol", New York Academy of Sciece, New York, New York (catalogue)</li>
+                </ul>
+            <h4>Group Exhibitions</h4>
+                <ul>
+                    <li>2020 “In Good Company”, Mana Contempory Chicago, Chicago, Illinois</li>
+                    <li>2019 “One Shot”, Manifest Gallery, Cincinnati, Ohio</li>
+                    <li>2018 “Got a Light?”, Assaf Evron, curator, The Suburban, Milwaukee</li>
+                    <li>2013-2014 “A Study In Midwestern Appropriation” Hyde Park Art Center, Michelle Grabner, curator, Chicago</li>
+                    <li>1998 “Hello Mr Soul”, Tony Tasset, curator, University of Illinois Chicago gallery, Chicago</li>
+                    <li>1992 Anonymous Museum, Chicago</li>
+                    <li>1989 “The Spectacle of Chaos”, special exhibition, Klaus Ottmann, curator, Chicago (catalogue)</li>
+                    <li>1988 “Function-non-Function”, Rezac gallery, Chicago (catalogue)</li>
+                    <li>1988 “Rites of Spring”, Rezac gallery, Chicago</li>
+                    <li>1988 “Gold”, Rezac gallery Chicago</li>
+                    <li>1987 “Information as Ornament”, Feature and Rezac gallery, Chicago, (catalogue with essay by David Sedaris)</li>
+                    <li>1987 “Chaos”, John Good Gallery, New York</li>
+                    <li>1987 “Chicago by the Square Foot”, Randolph Street Gallery, Chicago</li>
+                    <li>1987 “Sex, Death & Jello”, Randolph Street Gallery, Chicago</li>
+                    <li>1987 “The Non-Spiritual in Art: Abstract Painting: 1985-????”, special exhibition, Hudson, curator, Chicago (catalogue)</li>
+                </ul>
+        </div>
+
+        <div>
+            <h3>Publications, Articles, and Interviews</h3>
+                <ul>
+                    <li>Artner, Alan. “Catalog is best part of ‘Nonspiritual’.”. Chicago Tribune. June 11, 1987, section 5, p. 11</li>
+                    <li>Betsky, Aaron, Paul Florian and Stephen Wierzbowski. Violated Perfection. (No Title, inside cover image) New York: Rizzoli International Publications, Inc., 1990</li>
+                    <li>Bonesteel, Michael. “Medium Cool: New Chicago Abstraction.” Art In America. 75, no.12 December 1987 pp 138-47.</li>
+                    <li>Cunniff, Joseph. “Gold fish swallowing elevated to an art form.” Inside Lincoln Park. (April, 1990)</li>
+                    <li>Grabner, Michelle, “Spun” Catalog, November, 5, 2016</li>
+                    <li>Hixson, Kathryn and Kevin Maginnis. Catalog. Chicago: Anonymous Museum, 1992</li>
+                    <li>Holbert, Virginia. “Strange attractors shows intriguing theory of chaos.” The Chicago Sun-Times. October 6, 1989</li>
+                    <li>Jacobs, Tim. “Archetypal Images.” New City. October 11, 1989</li>
+                    <li>Lautman, Victoria. “The art of chaos.” Chicago Tribune. September 27, 1989, section 7 p. 7</li>
+                    <li>Maginnis, Kevin. Interview on National Public Radio. All Things Considered Weekend Edition. January 10, 1987</li>
+                    <li>Maginnis, Kevin. “Counter Reformation.” New Art Examiner. October 1987</li>
+                    <li>Maginnis, Kevin. “Concerning the non-spiritual in contemporary art.” Catalog essay. May 1987</li>
+                    <li>Maginnis, Kevin. “Order & Chaos, Contemporary Art and the Scientific Myth.” Catalog essay. December 1986</li>
+                    <li>McCracken, David. “Bring oder to the spectacle of chaos.” Chicago Tribune. September 8, 1989</li>
+                    <li>McCracken, David. Information as Ornament tests the limits.” Chicago Tribune. August 22, 1988, section 7 p. 59</li>
+                    <li>Nereim, Anders. “Whose muse, the spectacle of chaos.” Inland Architect. (January-February 1990)</li>
+                    <li>Pool, Robert. “Art Imitates Chaos.” Science. July 7 1989 vol.245, page 27</li>
+                    <li>Snodgrass, Susan. New Art Examiner. April 1988</li>
+                    <li>Westerbeck, Colin. “Pendants’ Progress.” Reader. October 1987</li>
+                    <li>Weinstein, Michael A. “Chadwick and Maginnis.” New City. March 29, 1990</li>
+                </ul>
+        </div>
+        </>
+    );
+};
+
+export default CvPage;
