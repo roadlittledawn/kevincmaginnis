36635141a23dfad47768a5ddd01cbfa9d0de30c3
Clinton <clinton.langosch@gmail.com>
2023-03-07 12:03:55 -0800

    feat: setup global styles and bring in new font

---
+++ src/components/GlobalStyles/GlobalStyles.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { normalize } from "polished";
+import { Global, css } from "@emotion/react";
+import typography from "./typography";
+import variables from "./variables";
+
+const GlobalStyles = () => (
+  <Global
+    styles={css`
+      ${normalize()}
+
+      :root {
+        ${variables};
+        ${typography};
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+
+      body {
+        font-size: 16px;
+        font-family: var(--primary-font-family);
+        color: var(--primary-text-color);
+        background-color: var(--primary-background-color);
+        line-height: 1.5;
+      }
+
+      a {
+        cursor: pointer;
+        color: var(--link-color);
+        transition: 0.2s ease-out;
+
+        &:hover {
+          color: var(--link-hover-color);
+        }
+      }
+
+      p {
+        margin-top: 0;
+        margin-bottom: var(--paragraph-spacing);
+        line-height: 1.75;
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+      }
+
+      h1 {
+        line-height: 1.15;
+        font-weight: bold;
+        margin-bottom: 1rem;
+      }
+
+      h2 {
+        line-height: 1.25;
+        margin-bottom: 0.75rem;
+        font-weight: 600;
+      }
+
+      h3 {
+        margin-bottom: 0.75rem;
+        font-weight: 600;
+      }
+    `}
+  />
+);
+
+export default GlobalStyles;
---
+++ src/components/GlobalStyles/typography.js
@@ -0,0 +1,7 @@
+import { css } from "@emotion/react";
+
+export default css`
+  --primary-font-family: "Nunito Sans", system-ui, sans-serif;
+
+  --paragraph-spacing: 1.25rem;
+`;
